ef3522f2eda61f72692febc9e6e0f05b1c9eb2cb
rsperlongo <ricardo_tadeu30@hotmail.com>
2021-11-09 15:20:37 -0300

    validation throw error

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
 
---
+++ src/tasks/dto/update-task-status.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { IsEnum } from 'class-validator';
+import { TaskStatus } from '../task.model';
+
+export class updateTaskStatusDto {
+  @IsEnum(TaskStatus)
+  status: TaskStatus;
+}
