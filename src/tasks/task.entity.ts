e53acbd787239b51c872d86dc94957e9e5c19bc6
Ricardo Sperlongo DECSKILL <RicardoSperlongo.DECSKILL@edp.com>
2022-02-22 16:31:57 -0300

    update task and insert interceptor

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { User } from './../auth/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -16,7 +17,8 @@ export class Task {
   @Column()
   status: string;
 
-  @ManyToOne(_type => User, user => user.tasks, { eager: false })
+  @ManyToOne((_type) => User, user => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User
 }
 
