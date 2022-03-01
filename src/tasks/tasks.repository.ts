833cd308baa7a16d8f78e9df5e62a2a777b24636
Ricardo Sperlongo DECSKILL <RicardoSperlongo.DECSKILL@edp.com>
2022-03-01 09:15:41 -0300

    update logger config

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,17 +1,21 @@
 /* eslint-disable prettier/prettier */
+import { InternalServerErrorException } from '@nestjs/common';
 import { User } from './../auth/user.entity';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
+import { Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository');
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
-    query.where({ user })
+    query.where({ user });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -19,13 +23,22 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
-        { search: `%${search}%`},
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
       );
     }
 
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user ${
+          user.username
+        }. Filters: ${JSON.stringify(filterDto)}, error.stack`,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
